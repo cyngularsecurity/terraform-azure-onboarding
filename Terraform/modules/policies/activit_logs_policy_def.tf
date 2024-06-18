795abd2178bc3987afe2e32f31557cc9cab4874a
dvir gross <dvirg@cyngularsecurity.com>
2024-06-18 21:51:53 +0300

    working basic aks policy def & assignment

--- Terraform/modules/policies/activit_logs_policy_def.tf
+++ Terraform/modules/policies/activit_logs_policy_def.tf
@@ -1,152 +1,164 @@
 
-# resource "azurerm_policy_definition" "activity_logs_diagnostic_settings" {
-#   name         = "cyngular-activity-logs"
-#   policy_type  = "Custom"
-#   mode         = "All"
-#   display_name = "apply activity logs per subscription"
-#   description  = "cyngular diagnostic settings deployment of subscription"
+resource "azurerm_policy_definition" "activity_logs_diagnostic_settings" {
+  name         = "cyngular-activity-logs"
+  policy_type  = "Custom"
+  mode         = "All"
+  display_name = "Cyngular ${var.client_name} subscription Activity logs"
+  description  = "cyngular diagnostic settings deployment for subscription Activity logs"
 
-#   metadata = jsonencode({ category = "Monitoring" })
+  metadata = jsonencode({ category = "Monitoring" })
+  parameters = jsonencode({
+    subscription = {
+      type = "String"
+      metadata = {
+        displayName = "Subscription ID"
+        description = "Id of subscription scope the policy will be assigned to"
+      }
+    }
+    StorageAccountID = {
+      type = "String"
+      metadata = {
+        displayName = "Storage Account"
+        description = "storage account ID"
+      }
+    }
+  })
 
-#   parameters = jsonencode({
-#     subscription = {
-#       type = "String"
-#       metadata = {
-#         displayName = "Subscription ID"
-#         description = "Id of subscription scope the policy will be assigned to"
-#       }
-#     }
-#     location = {
-#       type = "String"
-#       metadata = {
-#         displayName = "location"
-#         description = "Location where storage account will be deployed"
-#       }
-#     }
-
-#     storageAccounts = {
-#       type = "Object"
-#       metadata = {
-#         displayName = "Storage Accounts by Location"
-#         description = "A map of locations to lists of storage account IDs."
-#       }    }
-
-#     storageAccountID = {
-#       type = "String"
-#       metadata = {
-#         displayName = "Storage Account"
-#         description = "storage account ID"
-#       }
-#     }
-#   })
-
-#   policy_rule = jsonencode({
-#     if = {
-#       field  = "type"
-#       equals = "Microsoft.Resources/subscriptions"
-#     },
-#     then = {
-#       effect = "deployIfNotExists"
-#       details = {
-#         type = "Microsoft.Insights/diagnosticSettings"
-#         # "existenceCondition": {
-#         #   "allOf": [
-#         #     {
-#         #       "field": "Microsoft.Insights/diagnosticSettings/logs.enabled",
-#         #       "equals": "true"
-#         #     },
-#         #     {
-#         #       "field": "Microsoft.Insights/diagnosticSettings/logs.category",
-#         #       "equals": "Administrative"
-#         #     }
-#         #   ]
-#         # },
-#         roleDefinitionIds = [
-#           "/providers/Microsoft.Authorization/roleDefinitions/StorageAccountContributor",
-#           "/providers/Microsoft.Authorization/roleDefinitions/ccca81f6-c8dc-45e2-8833-a5e13f9ae238" // monitoring contributor
-#         ]
-#         deployment = {
-#           properties = {
-#             mode = "incremental"
-#             parameters = {
-#               subscription = {
-#                 value = "[parameters('subscription')]"
-#               }
-#               location = {
-#                 value = "[field('location')]"
-#               }
-#               storageAccountId = {
-#                 value = "[parameters('storageAccountID')]"
-#               }
-#             }
-#             template = {
-#               "$schema"      = "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#"
-#               contentVersion = "1.0.0.0"
-#               parameters = {
-#                 subscription = {
-#                   type = "string"
-#                 }
-#                 location = {
-#                   type = "string"
-#                 }
-#                 storageAccountId = {
-#                   type = "string"
-#                 }
-#               }
-#               resources = [
-#                 {
-#                   type       = "Microsoft.Insights/diagnosticSettings"
-#                   apiVersion = "2017-05-01-preview"
-#                   name       = "[concat(parameters('subscription'), '-activity-logs')]"
-#                   location   = "[parameters('location')]"
-#                   properties = {
-#                     # storageAccountId = "[parameters('storageAccountId')]"
-#                     storageAccountId = "[parameters('storageAccounts')[field('location')][0]]"
-#                     logs = [
-#                       {
-#                         category = "Recommendation"
-#                         enabled  = true
-#                         retentionPolicy = {
-#                           enabled = false
-#                           days    = 30
-#                         }
-#                       },
-#                       {
-#                         category = "Alert"
-#                         enabled  = true
-#                       },
-#                       {
-#                         category = "ServiceHealth"
-#                         enabled  = true
-#                       },
-#                       {
-#                         category = "Administrative"
-#                         enabled  = true
-#                       },
-#                       {
-#                         category = "Security"
-#                         enabled  = true
-#                       },
-#                       {
-#                         category = "Policy"
-#                         enabled  = true
-#                       },
-#                       {
-#                         category = "Autoscale"
-#                         enabled  = true
-#                       },
-#                       {
-#                         category = "ResourceHealth"
-#                         enabled  = true
-#                       }
-#                     ]
-#                   }
-#                 }
-#               ]
-#             }
-#           }
-#         }
-#       }
-#     }
-#   })
-# }
+  policy_rule = jsonencode({
+    if = {
+      field  = "type"
+      equals = "Microsoft.Resources/subscriptions"
+    },
+    then = {
+      effect = "deployIfNotExists"
+      details = {
+        type = "Microsoft.Insights/diagnosticSettings"
+        existenceCondition = {
+          allOf = [
+            {
+              field  = "Microsoft.Insights/diagnosticSettings/logs[*].enabled"
+              equals = "true"
+            },
+            {
+              field  = "Microsoft.Insights/diagnosticSettings/logs[*].category"
+              equals = "Administrative"
+            },
+            {
+              field  = "Microsoft.Insights/diagnosticSettings/logs[*].category"
+              equals = "Autoscale"
+            },
+            {
+              field  = "Microsoft.Insights/diagnosticSettings/logs[*].category"
+              equals = "Policy"
+            },
+            {
+              field  = "Microsoft.Insights/diagnosticSettings/logs[*].category"
+              equals = "Security"
+            },
+            {
+              field  = "Microsoft.Insights/diagnosticSettings/logs[*].category"
+              equals = "ServiceHealth"
+            },
+            {
+              field  = "Microsoft.Insights/diagnosticSettings/logs[*].category"
+              equals = "Alert"
+            },
+            {
+              field  = "Microsoft.Insights/diagnosticSettings/logs[*].category"
+              equals = "Recommendation"
+            },
+            {
+              field  = "Microsoft.Insights/diagnosticSettings/logs[*].category"
+              equals = "ResourceHealth"
+            }
+          ]
+        },
+        # roleDefinitionIds = [
+        #   "/providers/Microsoft.Authorization/roleDefinitions/StorageAccountContributor",
+        #   "/providers/Microsoft.Authorization/roleDefinitions/ccca81f6-c8dc-45e2-8833-a5e13f9ae238" // monitoring contributor
+        # ]
+        deployment = {
+          properties = {
+            mode = "incremental"
+            parameters = {
+              subscription = {
+                value = "[parameters('subscription')]"
+              }
+              location = {
+                value = "[field('location')]"
+              }
+              StorageAccountID = {
+                value = "[parameters('StorageAccountID')]"
+              }
+            }
+            template = {
+              "$schema"      = "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#"
+              contentVersion = "1.0.0.0"
+              parameters = {
+                subscription = {
+                  type = "string"
+                }
+                location = {
+                  type = "string"
+                }
+                StorageAccountID = {
+                  type = "string"
+                }
+              }
+              resources = [
+                {
+                  type       = "Microsoft.Insights/diagnosticSettings"
+                  apiVersion = "2017-05-01-preview"
+                  name       = "[concat(parameters('subscription'), '-activity-logs')]"
+                  location   = "[parameters('location')]"
+                  properties = {
+                    StorageAccountID = "[parameters('StorageAccountID')]"
+                    # StorageAccountID = "[parameters('storageAccounts')[field('location')][0]]"
+                    logs = [
+                      {
+                        category = "Recommendation"
+                        enabled  = true
+                        retentionPolicy = {
+                          enabled = false
+                          days    = 30
+                        }
+                      },
+                      {
+                        category = "Alert"
+                        enabled  = true
+                      },
+                      {
+                        category = "ServiceHealth"
+                        enabled  = true
+                      },
+                      {
+                        category = "Administrative"
+                        enabled  = true
+                      },
+                      {
+                        category = "Security"
+                        enabled  = true
+                      },
+                      {
+                        category = "Policy"
+                        enabled  = true
+                      },
+                      {
+                        category = "Autoscale"
+                        enabled  = true
+                      },
+                      {
+                        category = "ResourceHealth"
+                        enabled  = true
+                      }
+                    ]
+                  }
+                }
+              ]
+            }
+          }
+        }
+      }
+    }
+  })
+}
--- Terraform/modules/policies/remediation.tf
+++ Terraform/modules/policies/remediation.tf
@@ -1,12 +1,12 @@
 
-resource "azurerm_subscription_policy_remediation" "aks" {
-  for_each     = toset(var.subscriptions)
+# resource "azurerm_subscription_policy_remediation" "aks" {
+#   for_each     = toset(var.subscriptions)
 
-  name             = "cyngular-next-level-remediation"
-  subscription_id = "/subscriptions/${each.value}"
+#   name             = "cyngular-next-level-remediation"
+#   subscription_id = "/subscriptions/${each.value}"
   
-  policy_assignment_id = azurerm_subscription_policy_assignment.aks_diagnostic_settings[each.key].id
+#   policy_assignment_id = azurerm_subscription_policy_assignment.aks_diagnostic_settings[each.key].id
   
-  # location_filters = var.client_locations
-  # resource_count       = 100
-}
+#   # location_filters = var.client_locations
+#   # resource_count       = 100
+# }
