594ae1f37a082cdcfca73787aac3eb83bf1eb9cd
dvir gross <dvirg@cyngularsecurity.com>
2024-06-19 13:46:53 +0300

    better audit event and activity definition

--- Terraform/modules/policies/remediation.tf
+++ Terraform/modules/policies/remediation.tf
@@ -12,14 +12,14 @@ resource "azurerm_subscription_policy_remediation" "aks" {
   # resource_count       = 500
 }
 
-resource "azurerm_subscription_policy_remediation" "activity_logs" {
-  count        = var.enable_aks_logs ? 1 : 0
-  name            = "cyngular-${var.client_name}-aks-remediation"
-  subscription_id = "/subscriptions/${var.subscription}"
+# resource "azurerm_subscription_policy_remediation" "activity_logs" {
+#   count        = var.enable_aks_logs ? 1 : 0
+#   name            = "cyngular-${var.client_name}-aks-remediation"
+#   subscription_id = "/subscriptions/${var.subscription}"
 
-  policy_assignment_id = azurerm_subscription_policy_assignment.aks_diagnostic_settings[count.index].id
-  location_filters = var.client_locations
+#   policy_assignment_id = azurerm_subscription_policy_assignment.aks_diagnostic_settings[count.index].id
+#   location_filters = var.client_locations
   
-  failure_percentage = 1.0
-  parallel_deployments = 5
-}
+#   failure_percentage = 1.0
+#   parallel_deployments = 5
+# }
