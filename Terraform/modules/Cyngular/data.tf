a1b198a463d7d58b0848e42e6f47447860b1a4cd
dvir gross <dvirg@cyngularsecurity.com>
2024-07-22 04:01:09 +0300

    added tags to cyngular storage account on client side, should check for all svcs covered

--- Terraform/modules/Cyngular/data.tf
+++ Terraform/modules/Cyngular/data.tf
@@ -2,4 +2,11 @@ data "azuread_client_config" "current" {}
 
 locals {
     mgmt_group_id = data.azuread_client_config.current.tenant_id
+    storage_acount_tags     = {
+    cyngular-auditlogs = "" # TODO
+    cyngular-activitylogs = var.enable_activity_logs
+    cyngular-auditevents = var.enable_audit_events_logs
+    cyngular-nsgflowlogs = var.enable_flow_logs
+    cyngular-aks = var.enable_aks_logs
+  }
 }
