b525a95dee0cbbd433504b87f2b6e3c7c3044780
dvir gross <dvirg@cyngularsecurity.com>
2024-08-01 01:34:20 +0300

    func with local archive zip deploy

--- modules/function/app.tf
+++ modules/function/app.tf
@@ -11,22 +11,23 @@ resource "azurerm_linux_function_app" "function_service" {
   storage_account_name       = azurerm_storage_account.func_storage_account.name
   storage_account_access_key = azurerm_storage_account.func_storage_account.primary_access_key
 
-  zip_deploy_file = var.service_zip
+  zip_deploy_file = data.archive_file.function_app_zip.output_path // var.service_zip
+
   identity {
     type         = "UserAssigned"
     identity_ids = [azurerm_user_assigned_identity.function_assignment_identity.id]
   }
 
   app_settings = {
-    # "SCM_DO_BUILD_DURING_DEPLOYMENT" = true,
-    # "FUNCTIONS_EXTENSION_VERSION" = "~4",
-    "FUNCTIONS_WORKER_RUNTIME" = "python",
-    "WEBSITE_RUN_FROM_PACKAGE" = "https://cyngular-onboarding-templates.s3.amazonaws.com/azure/func_zip.zip"
+    # "SCM_DO_BUILD_DURING_DEPLOYMENT" = true
+    # "FUNCTIONS_EXTENSION_VERSION" = "~4"
+    "FUNCTIONS_WORKER_RUNTIME" = "python"
+    "WEBSITE_RUN_FROM_PACKAGE" = "https://cyngular-onboarding-templates.s3.amazonaws.com/azure/cyngular_func.zip"
 
-    "STORAGE_ACCOUNT_MAPPINGS" = jsonencode(var.default_storage_accounts),
-    "COMPANY_LOCATIONS"        = jsonencode(var.client_locations),
-    "ROOT_MGMT_GROUP_ID"       = local.mgmt_group_id,
-    "UAI_ID"                   = azurerm_user_assigned_identity.function_assignment_identity.client_id,
+    "STORAGE_ACCOUNT_MAPPINGS" = jsonencode(var.default_storage_accounts)
+    "COMPANY_LOCATIONS"        = jsonencode(var.client_locations)
+    "ROOT_MGMT_GROUP_ID"       = local.mgmt_group_id
+    "UAI_ID"                   = azurerm_user_assigned_identity.function_assignment_identity.client_id
 
     "enable_activity_logs"     = var.enable_activity_logs
     "enable_audit_events_logs" = var.enable_audit_events_logs
@@ -50,3 +51,43 @@ resource "azurerm_linux_function_app" "function_service" {
   }
   tags = var.tags
 }
+
+data "archive_file" "function_app_zip" {
+  type        = "zip"
+  source_dir  = "function_app"
+  output_path = "cyngular_func.zip"
+}
+
+# resource "azurerm_function_app_function" "example" {
+#   name            = "cyngular-function-app-function"
+#   function_app_id = azurerm_linux_function_app.function_service.id
+#   language        = "Python"
+
+#   file {
+#     name    = "run.csx"
+#     content = file("exampledata/run.csx")
+#   }
+
+#   test_data = jsonencode({
+#     "name" = "Azure"
+#   })
+#   config_json = jsonencode({
+#     "bindings" = [
+#       {
+#         "authLevel" = "function"
+#         "direction" = "in"
+#         "methods" = [
+#           "get",
+#           "post",
+#         ]
+#         "name" = "req"
+#         "type" = "httpTrigger"
+#       },
+#       {
+#         "direction" = "out"
+#         "name"      = "$return"
+#         "type"      = "http"
+#       },
+#     ]
+#   })
+# }
