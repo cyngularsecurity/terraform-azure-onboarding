66dedc36ed03a35cb57aca59eaf0cc13736f145f
dvir gross <dvirg@cyngularsecurity.com>
2024-08-04 12:49:33 +0300

    corrected path for func dir

--- modules/function/data.tf
+++ modules/function/data.tf
@@ -4,6 +4,6 @@ data "azuread_client_config" "current" {}
 data "archive_file" "function_app_zip" {
   type        = "zip"
   # source_dir  = "${path.module}/function_app"
-  source_dir  = "function_app"
+  source_dir  = "${path.root}/function_app"
   output_path = "cyngular_func.zip"
 }
