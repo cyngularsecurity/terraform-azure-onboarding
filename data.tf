14dac39ca1edfcaa906089d2629729ee0b7742ab
cyngularsecurity <tomerga@cyngularsecurity.com>
2024-08-15 14:50:41 +0300

    azurerm_management_group data

--- data.tf
+++ data.tf
@@ -1,3 +1,6 @@
 data "azuread_client_config" "current" {}
 data "azuread_application_published_app_ids" "well_known" {}
 data "azurerm_subscriptions" "available" {}
+data "azurerm_management_group" "root" {
+  name = "Tenant Root Group"
+}
