c5f5cc2f23e2f5dfca249b7167947fc680eda8db
dvir-uc <dvirg@cyngularsecurity.com>
2026-02-03 19:37:56 +0200

    DEVOPS-1001: Fix storage lifecycle policy by adding blob type filter and removing version action

--- modules/Cyngular/StorageAccount.tf
+++ modules/Cyngular/StorageAccount.tf
@@ -26,25 +26,23 @@ resource "azurerm_storage_account" "cyngular_sa" {
 }
 
 resource "azurerm_storage_management_policy" "cyngular_sa_lifecycle" {
-  for_each = azurerm_storage_account.cyngular_sa
+  for_each           = azurerm_storage_account.cyngular_sa
   storage_account_id = azurerm_storage_account.cyngular_sa[each.key].id
 
   rule {
     name    = "delete-after-${var.delete_retention_policy_days}-days"
     enabled = true
-    type    = "Lifecycle"
+
+    filters {
+      blob_types = ["blockBlob", "appendBlob"]
+    }
 
     actions {
       base_blob {
         delete_after_days_since_modification_greater_than = var.delete_retention_policy_days
-        # or: delete_after_days_since_creation_greater_than
       }
 
       snapshot {
-        delete_after_days_since_creation_greater_than = var.delete_retention_policy_days
-      }
-
-      version {
         delete_after_days_since_creation_greater_than = var.delete_retention_policy_days
       }
     }
